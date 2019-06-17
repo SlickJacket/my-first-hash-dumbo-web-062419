028c08bdbe8e6e090d6c92bf796350c5fb985687
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-17 01:59:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,7 +30,7 @@ def adding
 
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
-  shipping_manifest
+  return shipping_manifest
   # return the shipping_manifest hash below
 
 end
